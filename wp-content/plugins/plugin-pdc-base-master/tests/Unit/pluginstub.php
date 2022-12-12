74b212c839f1db33baff8bf6cb2fd380b704d40d
Robbert Broersma <git@robbertbroersma.nl>
2022-12-12 19:26:12 +0100

    chore: remove old OpenPDC

--- wp-content/plugins/plugin-pdc-base-master/tests/Unit/pluginstub.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Plugin Name:       Plugin Stub
- * Plugin URI:        https://www.openwebconcept.nl/
- * Version:           1.1.5
- *
- * This file is used for testing the dependency checkere (determining Version from Plugin DocBlock).
- */
